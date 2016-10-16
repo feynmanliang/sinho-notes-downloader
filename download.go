a1a24674dafced6c437f53ec7c13363df7f8f7d9
Feynman Liang <feynman.liang@gmail.com>
2016-10-15 19:27:09 -0700

    Use pipeline + fanout

--- download.go
+++ download.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"sync"
 )
 
 type crawlResult struct {
@@ -16,79 +17,110 @@ type crawlResult struct {
 var gDrivePrefixRE = regexp.MustCompile("https://drive.google.com/open\\?id=")
 var slashRE = regexp.MustCompile("/")
 
-func crawlLinks(url string, results chan<- crawlResult, jobFinished chan<- bool) {
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
+func crawlLinks(url string) <-chan crawlResult {
+	results := make(chan crawlResult)
+	go func() {
+		resp, _ := http.Get(url)
+		defer resp.Body.Close()
 
-	z := html.NewTokenizer(resp.Body)
-	var className string
-	for {
-		tt := z.Next()
+		z := html.NewTokenizer(resp.Body)
+		var className string
+		for {
+			tt := z.Next()
 
-		switch tt {
-		case html.ErrorToken:
-			// End of the document, we're done
-			close(results)
-			return
-		case html.StartTagToken:
-			t := z.Token()
-			switch t.Data {
-			case "h3":
-				z.Next()
-				className = slashRE.ReplaceAllString(z.Token().Data, "-")
-			case "a":
-				for _, a := range t.Attr {
-					if gDrivePrefixRE.MatchString(a.Val) {
-						url := a.Val
-						gDocId := gDrivePrefixRE.ReplaceAllString(url, "")
+			switch tt {
+			case html.ErrorToken:
+				// End of the document, we're done
+				close(results)
+				return
+			case html.StartTagToken:
+				t := z.Token()
+				switch t.Data {
+				case "h3":
+					z.Next()
+					className = slashRE.ReplaceAllString(z.Token().Data, "-")
+				case "a":
+					for _, a := range t.Attr {
+						if gDrivePrefixRE.MatchString(a.Val) {
+							url := a.Val
+							gDocId := gDrivePrefixRE.ReplaceAllString(url, "")
 
-						z.Next()
-						title := z.Token().Data
+							z.Next()
+							title := z.Token().Data
 
-						results <- crawlResult{className, title, gDocId}
+							results <- crawlResult{className, title, gDocId}
+						}
 					}
 				}
 			}
 		}
-	}
+		close(results)
+	}()
+	return results
 }
 
-func downloadCrawlResult(result crawlResult, jobFinished chan<- bool) {
-	downloadUrl := fmt.Sprintf(
-		"https://drive.google.com/uc?export=download&id=%s",
-		result.gDocId)
+func downloadCrawlResult(results <-chan crawlResult) <-chan string {
+	messages := make(chan string)
+	go func() {
+		for result := range results {
+			downloadUrl := fmt.Sprintf(
+				"https://drive.google.com/uc?export=download&id=%s",
+				result.gDocId)
 
-	os.MkdirAll(fmt.Sprintf("downloads/%s", result.className), 0777)
-	f, _ := os.Create(fmt.Sprintf("downloads/%s/%s.pdf", result.className, result.title))
-	defer f.Close()
+			os.MkdirAll(fmt.Sprintf("downloads/%s", result.className), 0777)
+			f, _ := os.Create(fmt.Sprintf("downloads/%s/%s.pdf", result.className, result.title))
+			defer f.Close()
 
-	resp, _ := http.Get(downloadUrl)
-	defer resp.Body.Close()
+			resp, _ := http.Get(downloadUrl)
+			defer resp.Body.Close()
 
-	io.Copy(f, resp.Body)
-	jobFinished <- true
-}
-
-func downloadAllNotes(url string) {
-	results := make(chan crawlResult, 8)
-	jobFinished := make(chan bool)
-	go crawlLinks(url, results, jobFinished)
-
-	numJobs := 0
-	for result := range results {
-		numJobs++
-		fmt.Printf("Downloading %s %s\n", result.className, result.title)
-		go downloadCrawlResult(result, jobFinished)
-	}
-	for numJobs > 0 {
-		select {
-		case <-jobFinished:
-			numJobs--
+			io.Copy(f, resp.Body)
+			messages <- fmt.Sprintf("Downloaded %s %s", result.className, result.title)
 		}
+		close(messages)
+	}()
+	return messages
+}
+
+func downloadAllNotes(url string, numWorkers uint8) {
+	results := crawlLinks(url)
+	m := make([]<-chan string, numWorkers)
+	for i := range m {
+		m[i] = downloadCrawlResult(results)
 	}
+	for message := range merge(m...) {
+		fmt.Println(message)
+	}
+}
+
+func merge(cs ...<-chan string) <-chan string {
+	var wg sync.WaitGroup
+	out := make(chan string)
+
+	// Start an output goroutine for each input channel in cs.  output
+	// copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan string) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
 
 func main() {
 	url := "https://chewisinho.github.io"
-	downloadAllNotes(url)
+	numWorkers := uint8(128)
+	downloadAllNotes(url, numWorkers)
 }
